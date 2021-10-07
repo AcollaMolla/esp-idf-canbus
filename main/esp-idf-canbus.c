6909b2d25b53f07b322f53d9123fa4202e9c1c89
Anton <antengman@gmail.com>
2021-10-07 21:36:14 +0200

    Set up twai driver. Import header files.

--- main/esp-idf-canbus.c
+++ main/esp-idf-canbus.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 #include "driver/twai.h"
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+static const char* TAG_TWAI = "CAN";
+
+void setup_twai_driver(void){
+    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_21, GPIO_NUM_22, TWAI_MODE_NORMAL);
+    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_125KBITS();
+    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+
+    if(twai_driver_install(&g_config, &t_config, &f_config) == ESP_OK){
+        ESP_LOGI(TAG_TWAI, "Driver installed!\n");
+    }else{
+        ESP_LOGI(TAG_TWAI, "Failed to install driver\n");
+    }
+
+    //Start TWAI driver
+    if(twai_start() == ESP_OK){
+        ESP_LOGI(TAG_TWAI, "Driver started!\n");
+    }else{
+        ESP_LOGI(TAG_TWAI, "Failed to start driver\n");
+    }
+}
 
 void app_main(void)
 {
